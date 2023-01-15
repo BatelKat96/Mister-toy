a47d2be65728a219dc73936d7cfd25cc668c2ece
BatelKat96 <batelkat@gmail.com>
2023-01-15 20:13:56 +0200

    Create atlas db

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName: 'ToyBD'
+  // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://batel:batel1996@cluster0.hihqtmp.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'mister_toy_DB'
 }
