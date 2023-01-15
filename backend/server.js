552dada2f03bf5747a504c67777bbb8b743b9c8d
BatelKat96 <batelkat@gmail.com>
2023-01-15 17:39:06 +0200

    Create build folder

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
-    // app.use(express.static('public'))
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
@@ -34,6 +33,9 @@ app.use('/api/user', userRoutes)
 app.use('/api/toy', toyRoutes)
 
 
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
